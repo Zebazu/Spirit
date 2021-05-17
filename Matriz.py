1fb478c5c08c507593a0dbc9a0fb67d6033786f8
PatricioAvalos <patricio.avalos@alumnos.ucn.cl>
2021-05-16 23:03:24 -0400

    Matriz y Terreno Añadidos

---
+++ Matriz.py
@@ -0,0 +1,11 @@
+from Terreno import Terreno
+import random
+import numpy as np
+
+class Matriz:
+    def __init__(self,x,y):
+        self.matriz = np.empty((x,y))
+
+    def generarArea(self):
+        #implementar random para crear terrenos
+        return None
---
+++ Terreno.py
@@ -0,0 +1,35 @@
+class Terreno:
+
+    def __init__(self,tipo,obstaculo):
+        self.tipo = tipo
+        self.obstaculo = obstaculo
+    
+    def getTipoTerreno(self):
+        return self.tipo
+
+    def getObstaculosTerreno(self):
+        return self.obstaculo
+
+    def getDetalleTerreno(self):
+        return ("Terreno Llano") if self.tipo == 0 else ("Terreno Abrupto")
+
+    def getDetalleObstaculo(self):
+        detalle = ""
+        hayObstaculos = False
+        for i in range(4):
+            if self.obstaculo[i] == 1 and i == 0:
+                detalle+= "Obstaculo Superior"
+                detalle+= "; "
+                hayObstaculos = True
+            elif self.obstaculo[i] == 1 and i == 1:
+                detalle+= "Obstaculo Costado Derecho"
+                detalle+= "; "
+                hayObstaculos = True
+            elif self.obstaculo[i] == 1 and i == 2:
+                detalle+= "Obstaculo Inferior"
+                detalle+= "; "
+                hayObstaculos = True
+            elif self.obstaculo[i] == 1 and i == 3:
+                detalle+= "Obstaculo Costado Izquierdo"
+                hayObstaculos = True
+        return detalle if hayObstaculos else "No hay Obstaculos"    
